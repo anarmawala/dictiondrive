4d30594aca90e6ba7d5e94f846fd94d855b35dda
anarmawala <arshadn00@gmail.com>
2017-11-11 14:03:13 -0600

    New Changes

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var mid = require('./routes/mid');
+var config = require('./config');
+
 
 var app = express();
 
@@ -22,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', mid);
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var config = require('../config');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('data', config.about);
+});
+
+/* GET legal page. */
+router.get('/legal', function(req, res, next) {
+  res.render('data', config.legal);
 });
 
 module.exports = router;
